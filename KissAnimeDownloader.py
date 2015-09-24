c274b98dc8405938ac6979dc785c0e1494281a64
Aditya A Prasad <adityaprasadiskool@gmail.com>
2015-09-24 14:55:57 -0700

    Finished Script
    I guess when I updated all my python pakages
    my selenium got to cheat the cloudflare server.
    
    The delay of 10 seconds is a little steep. Ill try to find
    a different way, I tried the following
    
    This didnt work as there about 5 seconds during which CloudFlare transfers
    us to the actual site. so "driver.find_element_by_id('head')" wont work
    
    from selenium.webdriver.support.ui import WebDriverWait
    from selenium.webdriver.support import expected_conditions as EC
    from selenium.common.exceptions import TimeoutException
    delay = 10 # seconds
    try:
        WebDriverWait(driver, delay).until(EC.presence_of_element_located(driver.find_element_by_id('head')))
        print "Page is ready!"
    except TimeoutException:
        print "Loading took too much time!"
    
    the point is we need to wait for some elements of the page to load.
    This should be possible, we dont even need to wait for all the ajax requests to
    complete. the adverts and all take so much time.
    
    A work around I found is to stop the loading in the firefox browser after it seems to have loaded.
    I encountered cases where the page seemed to be loading for ever!!
    
    Other than that, the blogspot links shouldnt be taken, the option of choosing anime
    and quailty of download should be added.
    
    Signed-off-by: Aditya A Prasad <adityaprasadiskool@gmail.com>

--- KissAnimeDownloader.py
+++ KissAnimeDownloader.py
@@ -1,41 +1,99 @@
 # KISSANIME - http://kissanime.com/ ANIME DOWNLOADER
-import urllib, urllib2, httplib
-httplib.HTTPConnection.debuglevel = 1
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 BASE_URL = "http://kissanime.com/Anime/"
-# EDIT THIS AND ADD YOUR REQUIRED ANIME NAME
-AnimeName = "Nodame-Cantabile"
-URL = BASE_URL + AnimeName
-
-
+DELAY = 10 # change it depending on your internet connectivity
 episodeURLs = []
 downloadURLs = []
 
-def getDownloadURLs(url):
-    print url
-    driver = webdriver.Firefox()
-    driver.get(url)
-# because they block scrapers, we use "magic browser"! lol
-req = urllib2.Request(URL, headers={'User-Agent' : "Magic Browser"})
+#------------------------------- EDIT THIS AND ADD YOUR REQUIRED ANIME NAME
+AnimeName = "Nodame-Cantabile"
+#-------------------------------
 
-con = urllib2.urlopen(req)
+URL = BASE_URL + AnimeName
 
-soup = BeautifulSoup(con)
+print "Opening firefox Browser"
+driver = webdriver.Firefox()
 
-# gets all the tables
-tables = soup.findAll('td')
+print "Navigating to Login Page"
+driver.get("http://kissanime.com/Login")
 
-# we go through the tables
-for table in tables:
+print "DELAY start"
+time.sleep(DELAY)
+print "DELAY end"
+
+print "Logging in"
+user = driver.find_element_by_name("username")
+passwd = driver.find_element_by_name("password")
+user.send_keys("<ur username>")
+passwd.send_keys("<ur password>")
+passwd.send_keys(Keys.RETURN)
+
+print "DELAY start"
+time.sleep(DELAY)
+print "DELAY end"
+
+print "Navigating to anime episode page"
+driver.get(URL)
+
+print "DELAY start"
+time.sleep(DELAY)
+print "DELAY end"
+
+html = driver.page_source
+soup = BeautifulSoup(html)
+epListTable = soup.find("table", {"class" : "listing"})
+
+for row in epListTable.findAll('tr'):
+    # each row is <td> tag enclosed
     try:
-        # whenever we get a 'a' tag we extract the 'href' attribute
-        episodeURLs.append(table.findAll('a')[0].get('href'))
-    # In every alternate line no 'a' exists trying to access the
-    # first element ([0]) of an empty list results in this
+        episodeURLs.append("http://kissanime.com"+row.findAll('a')[0].get('href'))
     except IndexError:
-        pass
+        print "\n Obtaining episode URL's ....\n"
 
-for episodeURL in episodeURLs:
-    downloadURLs = getDownloadURLs("http:/kissanime.com/Anime" + episodeURL)
+print "These are the episode URL's"
+print episodeURLs
+
+for url in episodeURLs:
+    print "\n Navigating to get Video for the URL => "+url
+    driver.get(url)
+
+    print "DELAY start"
+    time.sleep(DELAY)
+    print "DELAY end"
+
+    temp = []
+    
+    html = driver.page_source
+    soup = BeautifulSoup(html)
+    for div in soup.findAll('div', {"id" : "divDownload"}):
+        links = div.findAll('a')
+        for link in links:
+            dummy = (url[url.find('?')-2:url.find('?')], link.text.strip(), link.attrs['href'])
+            temp.append(dummy)
+            print "\n\n Temp for"+link.text.strip()
+            print temp
+            
+    downloadURLs.append(temp)
+    
+for link in downloadURLs:
+    print link
+    print "\n"
+
+
+print "Copy paste the above links to a text file, use import from tezt file option in IDM to download all"
+
+
+            
+            
+    
+    
+
+
+
+
+        
